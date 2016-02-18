38ebbf77078f55d8ffdb9db0c410e15592c2330a
Jouke Waleson <jouke.waleson@mendix.com>
2016-02-18 15:57:01 +0100

    add 5.15.0

--- runtime/mxclientsystem/reporting/store/ParameterStore.js
+++
@@ -1,7 +0,0 @@
-/*
-    Copyright (c) 2005-2015, Mendix bv. All rights reserved.
-    See licenses.txt for third party licenses that apply.
-*/
-
-//>>built
-define("reporting/store/ParameterStore",[],function(){var _1={};var _2={};return {registerParams:function(id,_3){_1[id]=_3;if(_2[id]){for(var i=0;i<_2[id].length;i++){_2[id][i](_3);}_2[id]=null;}},retrieveParams:function(id,_4){if(_1[id]){_4(_1[id]);}else{if(!_2[id]){_2[id]=[];}_2[id].push(_4);}}};});
