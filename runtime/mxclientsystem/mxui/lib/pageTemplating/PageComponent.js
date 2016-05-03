c2003386fc7f0b41ed26c45d79c3d4e3a4b95ec2
Jouke Waleson <jouke.waleson@mendix.com>
2016-05-03 15:59:40 +0200

    add 6.5.0

--- runtime/mxclientsystem/mxui/lib/pageTemplating/PageComponent.js
+++ runtime/mxclientsystem/mxui/lib/pageTemplating/PageComponent.js
@@ -4,4 +4,4 @@
 */
 
 //>>built
-define("mxui/lib/pageTemplating/PageComponent",["dojo/_base/array","dojo/_base/lang"],function(_1,_2){"use strict";function _3(id,_4,_5){this._id=id;this._content=_4;this.isMain=_5;};_3.prototype.findPlaceholder=function(_6){return _6.querySelector("[data-mx-placeholder='"+this._id+"']");};_3.prototype.appendTo=function(_7){_1.forEach(this._content,_2.hitch(_7,_7.appendChild));};return _3;});
+define("mxui/lib/pageTemplating/PageComponent",["dojo/_base/array","dojo/_base/lang"],function(_1,_2){"use strict";function _3(id,_4){this._id=id;this._content=_4;};_3.prototype.findPlaceholder=function(_5){return _5.querySelector("[data-mx-placeholder='"+this._id+"']");};_3.prototype.appendTo=function(_6){_1.forEach(this._content,_2.hitch(_6,_6.appendChild));};return _3;});
