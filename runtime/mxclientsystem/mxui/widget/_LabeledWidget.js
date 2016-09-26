c419cb087eb2d15381a573d98f28250d41215c14
Jouke Waleson <jouke.waleson@mendix.com>
2016-09-26 18:13:22 +0200

    add 6.9.0

--- runtime/mxclientsystem/mxui/widget/_LabeledWidget.js
+++ runtime/mxclientsystem/mxui/widget/_LabeledWidget.js
@@ -4,4 +4,4 @@
 */
 
 //>>built
-define("mxui/widget/_LabeledWidget",["mxui/widget/_WidgetBase","dojo/_base/array","dojo/_base/declare"],function(_1,_2,_3){var _4=_3(_1,{declaredClass:"mxui.widget._LabeledWidget",insideFormGroup:false,startup:function(){if(!this.insideFormGroup){return;}var _5=this.domNode.querySelectorAll("input,textarea,select");if(_5.length>1){return;}var _6=_5[0]||this.domNode.querySelector("button");if(!_6.id){_6.id=this.id+"_input";}var _7=this.domNode.previousSibling;_7.setAttribute("for",_6.id);}});return _4;});
+define("mxui/widget/_LabeledWidget",["mxui/widget/_WidgetBase","dojo/_base/array","dojo/_base/declare"],function(_1,_2,_3){var _4=_3(_1,{declaredClass:"mxui.widget._LabeledWidget",insideFormGroup:false,startup:function(){if(!this.insideFormGroup){return;}var _5=/input|textarea|select/i.test(this.domNode.nodeName)?this.domNode:null;if(!_5){var _6=this.domNode.querySelectorAll("input,textarea,select");if(_6.length>1){return;}_5=_6[0];}if(!_5){_5=this.domNode.querySelector("button");}if(!_5.id){_5.id=this.id+"_input";}var _7=this.domNode.previousElementSibling;if(_7&&_7.nodeName==="LABEL"){_7.setAttribute("for",_5.id);}}});return _4;});
