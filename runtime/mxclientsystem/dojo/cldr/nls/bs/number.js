6dc0e9e2700e5b9ed4688325e4079ddb54c394f8
Mendix Runtime Packer <yolo@mendix.com>
2017-08-07 14:48:17 +0200

    add 7-build23144

--- runtime/mxclientsystem/dojo/cldr/nls/bs/number.js
+++ runtime/mxclientsystem/dojo/cldr/nls/bs/number.js
@@ -6,4 +6,3 @@
 
 //>>built
 define("dojo/cldr/nls/bs/number",{group:".",decimal:","});
-//# sourceMappingURL=number.js.map
--- runtime/mxclientsystem/dojo/errors/CancelError.js
+++ runtime/mxclientsystem/dojo/errors/CancelError.js
@@ -6,4 +6,3 @@
 
 //>>built
 define("dojo/errors/CancelError",["./create"],function(a){return a("CancelError",null,null,{dojoType:"cancel",log:!1})});
-//# sourceMappingURL=CancelError.js.map
