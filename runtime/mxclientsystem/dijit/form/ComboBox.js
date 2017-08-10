57b6e50fbe8972f71b0555129c65ecd53e085ce2
Mendix Runtime Packer <yolo@mendix.com>
2017-08-10 11:35:40 +0200

    add 7-build23186

--- runtime/mxclientsystem/dijit/form/ComboBox.js
+++ runtime/mxclientsystem/dijit/form/ComboBox.js
@@ -1,3 +1,2 @@
 //>>built
 define("dijit/form/ComboBox",["dojo/_base/declare","./ValidationTextBox","./ComboBoxMixin"],function(a,b,c){return a("dijit.form.ComboBox",[b,c],{})});
-//# sourceMappingURL=ComboBox.js.map
--- runtime/mxclientsystem/dojo/data/api/Item.js
+++ runtime/mxclientsystem/dojo/data/api/Item.js
@@ -6,4 +6,3 @@
 
 //>>built
 define("dojo/data/api/Item",["../../_base/declare"],function(a){return a(null,{})});
-//# sourceMappingURL=Item.js.map
