63e98a2a6739ae8dfb6314fe29ead375b8921deb
tao <tao.zhang@mendix.com>
2017-09-08 14:18:42 +0200

    add 6.10.10

--- runtime/mxclientsystem/dojo/cldr/nls/en-au/number.js
+++ runtime/mxclientsystem/dojo/cldr/nls/en-au/number.js
@@ -6,3 +6,4 @@
 
 //>>built
 define("dojo/cldr/nls/en-au/number",{currencyFormat:"\u00a4#,##0.00"});
+//# sourceMappingURL=number.js.map
--- runtime/mxclientsystem/dojo/errors/RequestError.js
+++ runtime/mxclientsystem/dojo/errors/RequestError.js
@@ -6,3 +6,4 @@
 
 //>>built
 define("dojo/errors/RequestError",["./create"],function(a){return a("RequestError",function(a,b){this.response=b})});
+//# sourceMappingURL=RequestError.js.map
