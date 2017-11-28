8b8e67e5cca5d13b87379c8214631b54d4bf3ad9
Mendix Runtime Packer <yolo@mendix.com>
2017-11-28 10:23:56 +0100

    add 7-build25039

---
+++ modeler/tools/node/rn-cli.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extraNodeModules: {
+      "react-native": __dirname + "/node_modules/react-native",
+      "react": __dirname + "/node_modules/react",
+      "mendix/native": __dirname + "/node_modules/mendix/native.js"
+    }
+}
